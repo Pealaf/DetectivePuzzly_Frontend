29fbfe4040aedd7ccf35f1da7369012133f1d988
MV <mathilde.vollet.auditeur@lecnam.net>
2024-04-05 00:04:43 +0200

    Ajout d'un bouton de déconnexion dans le header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,16 @@
-import { Link } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import logo from '../../assets/logo.jpg';
 import '../../styles/header.css';
+import React from "react";
  
 function Header() {
+    const navigate = useNavigate();
+
+    const deconnexion = () => {
+        localStorage.removeItem('token');
+        navigate('/survey');
+    };
+
     return (
         <nav className='header'>
             <Link to="/home" className='lien-logo'>
@@ -11,6 +19,8 @@ function Header() {
             <Link to="/home" className='lien'>Accueil</Link>
             <Link to="/profil" className='lien'>Mon profil</Link>
             <Link to="/survey" className='lien'>test</Link>
+            <button onClick={deconnexion}><i className="fa-solid fa-right-from-bracket"></i></button>
+
         </nav>
     );
 }
--- src/pages/Profil/checkAuthentification.js
+++ src/pages/Profil/checkAuthentification.js
@@ -2,12 +2,15 @@ import { Navigate} from 'react-router-dom';
 import Header from "../../components/Header";
 
 function CheckAuthentification() {
+    let url = window.location.href;
+    let decoupe = url.split(/[/]/);
+    let page = decoupe[decoupe.length-1];
     if(!localStorage.hasOwnProperty('token')) {
-        return <Navigate to="/profil" replace={true} />
+        if(page !== "nouveauProfil")
+            return <Navigate to="/profil" replace={true} />
     } else {
-        let url = window.location.href;
-        let decoupe = url.split(/[/]/);
-        if(decoupe[decoupe.length-1] === "profil" || decoupe[decoupe.length-1] === "nouveauProfil") {
+
+        if(page === "profil" || page === "nouveauProfil") {
             return <Navigate to="/home" replace={true} />
         }
         return <Header/>
