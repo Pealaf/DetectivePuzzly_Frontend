8cb84939e1078e28d5dff727ebf470b799cb44bd
MV <mathilde.vollet.auditeur@lecnam.net>
2024-05-11 19:52:45 +0200

    Implémentation de la génération d'énigmes.

--- src/pages/Enigme/index.jsx
+++ src/pages/Enigme/index.jsx
@@ -1,27 +1,91 @@
-import CheckAuthentification from "../Profil/checkAuthentification";
-import Input from "../../components/Input";
-import React, {useState} from "react";
 import Button from "../../components/Button";
+import {Navigate, useNavigate} from "react-router-dom";
+import { useState, useEffect } from "react";
+import {checkAuthentification} from "../../utils/utilsFunctions";
+import Header from "../../components/Header";
 
 function PageEnigme() {
 
-    const [reponse, setReponse] = useState('');
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(true);
+    const [enigme, setEnigme] = useState(null);
 
-    return (
-        <div className="divPrincipale">
-            <header>
-                {CheckAuthentification()}
-            </header>
-            <div className="divContent">
-                <div className="divConnexion">
-                    <h1>Qu'est-ce qui peut être dans la mer et dans le ciel ?</h1>
-                    <Input type="text" name="reponse" label="Réponse" value={reponse} setValue={setReponse} />
-                    <Button intitule="Valider" />
+    useEffect(() => {
+        const genererEnigme = async () => {
+            let idCurrentUser = JSON.parse(localStorage.getItem("currentUser"))["id"];
+            let token = localStorage.getItem("token");
+
+            try {
+                const response = await fetch("http://localhost:8000/api/enigmes/generate/" + idCurrentUser, {
+                    method: "GET",
+                    mode: "cors",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": "bearer " + token
+                    }
+                });
+
+                if (!response.ok) {
+                    if (response.status === 401) {
+                        localStorage.removeItem('token');
+                        localStorage.removeItem('currentUser');
+                        navigate('/profil');
+                    } else {
+                        console.error('Erreur HTTP, statut : ' + response.status);
+                        throw new Error('Erreur HTTP');
+                    }
+                }
+
+                const jsonResponse = await response.json();
+                setEnigme(jsonResponse);
+            } catch (error) {
+                console.error('Une erreur est survenue :', error);
+            } finally {
+                setLoading(false); // À la fin de la requête, on arrête le chargement
+            }
+        };
+
+        genererEnigme();
+    }, [navigate]);
+
+    // Vérification de la connexion
+    if(!checkAuthentification()) {
+        return <Navigate to="/profil" replace={true} />;
+    } else {
+        return (
+            <div className="divPrincipale">
+                <header>
+                    <Header/>
+                </header>
+
+                <div className="divContent">
+                    <div className="divConnexion">
+                        {loading ? (
+                            <p>Chargement en cours...</p>
+                        ) : (
+                            <>
+                                {enigme && (
+                                    <>
+                                        <h2>{enigme.intitule}</h2>
+                                        <div id="divBoutons">
+                                            <div className="divLigneBoutons">
+                                                <Button intitule={enigme["reponse_a"]}/>
+                                                <Button intitule={enigme["reponse_b"]}/>
+                                            </div>
+                                            <div className="divLigneBoutons">
+                                                <Button intitule={enigme["reponse_c"]}/>
+                                                <Button intitule={enigme["reponse_d"]}/>
+                                            </div>
+                                        </div>
+                                    </>
+                                )}
+                            </>
+                        )}
+                    </div>
                 </div>
             </div>
-
-        </div>
-    );
+        );
+    }
 }
 
 export default PageEnigme;
