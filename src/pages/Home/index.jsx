29f46e7f06c939e71f86938d11ac6030e9a1c633
MV <mathilde.vollet.auditeur@lecnam.net>
2024-05-13 17:18:53 +0200

    Page d'accueil.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,24 +1,89 @@
 import Header from "../../components/Header";
-import {checkAuthentification} from "../../utils/utilsFunctions";
-import {Navigate} from "react-router-dom";
+import {checkAuthentification, resetLocalStorage} from "../../utils/utilsFunctions";
+import {Navigate, useNavigate} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import Button from "../../components/Button";
 
 function Home() {
+
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        setLoading(true);
+        getInfos();
+    }, [navigate]);
+
+    const getInfos = async () => {
+        let token = localStorage.getItem("token");
+        let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+        try {
+            const response = await fetch("http://localhost:8000/api/enigmes/count/" + currentUser.id, {
+                method: "GET",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "bearer " + token
+                }
+            });
+
+            if (!response.ok) {
+                if (response.status === 401) {
+                    resetLocalStorage();
+                    navigate('/profil');
+                } else {
+                    console.error('Erreur HTTP, statut : ' + response.status);
+                    throw new Error('Erreur HTTP');
+                }
+            }
+
+            const data = await response.json();
+
+            data["login"] = currentUser.login;
+            data["pourcentageReussite"] = Math.round(data["nombreEnigmesResolues"]*100/data["nombreEnigmes"]);
+
+            setData(data);
+        } catch (error) {
+            console.error('Une erreur est survenue :', error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const afficherPageNouvelleEnigme = async () => {
+        navigate('/enigme');
+    }
 
     // Vérification de la connexion
     if(!checkAuthentification()) {
         return <Navigate to="/profil" replace={true} />;
     } else {
-        let currentUserLogin = JSON.parse(localStorage.getItem("currentUser"))["username"];
-
         return (
             <div className="divPrincipale">
                 <header>
                     <Header/>
                 </header>
                 <div className="divContent">
-                    <p>
-                        Bonjour {currentUserLogin}
-                    </p>
+                    <div className="divConnexion">
+                        {loading ? (
+                            <p>Chargement en cours...</p>
+                        ) : (
+                            <>
+                                {data && (
+                                    <>
+                                        <h2>Bonjour {data["login"]} !</h2>
+                                        <p>Nombre d'énigmes résolues : {data["nombreEnigmesResolues"]}</p>
+                                        <p>Nombre d'énigmes total : {data["nombreEnigmes"]}</p>
+                                        <p>Pourcentage de réussite : {data['pourcentageReussite']} %</p>
+                                        <br/>
+                                        <Button intitule="Nouvelle énigme" onClick={() => afficherPageNouvelleEnigme()}/>
+                                    </>
+                                )}
+                            </>
+                        )}
+                    </div>
                 </div>
             </div>
         );
